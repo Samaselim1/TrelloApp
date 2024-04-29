eb2182db9dce6f35a1f9df59fede41eb24f12e4e
ahmedcanoo <149202243+ahmedcanoo@users.noreply.github.com>
2024-04-29 16:49:32 +0300

    Update UserController.java

--- Controller/UserController.java
+++ Controller/UserController.java
@@ -1,5 +1,19 @@
 package Controller;
-
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import ejb.User; 
+@Path("/users")
 public class UserController {
-
+    @POST
+    @Path("/register")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response registerUser(User user) {
+    	UserService us =new UserService();
+    	us.register(user);
+        return Response.ok().build();
+    }
 }
