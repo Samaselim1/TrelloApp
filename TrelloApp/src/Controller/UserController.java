45f3cf17f10480c5d0357d2e98e91cced5f93cdd
ahmedcanoo <149202243+ahmedcanoo@users.noreply.github.com>
2024-04-29 18:31:16 +0300

    Update UserController.java

--- TrelloApp/src/Controller/UserController.java
+++ TrelloApp/src/Controller/UserController.java
@@ -1,5 +1,43 @@
 package Controller;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import Service.UserService;
+import Model.User; 
+@Path("/users")
 public class UserController {
-
+    @POST
+    @Path("/register")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response registerUser(User user) {
+    	UserService us =new UserService();
+    	us.registerUser(user);
+        return Response.ok().build();
+    }
+    @POST
+    @Path("/login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response loginUser(User credentials) {
+        UserService userService = new UserService();
+        boolean authenticated = userService.login(credentials);
+        if (authenticated) {
+            return Response.ok().build();
+        } else {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+    }
+    @POST
+    @Path("/update")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateUser(User user) {
+        UserService userService = new UserService();
+        userService.update(user);
+        return Response.ok().build();
+    }
 }
