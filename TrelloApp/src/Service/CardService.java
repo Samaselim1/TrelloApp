312f8dd469b811ec5e22421dcbe62ba6325e7121
Samaselim1 <125610375+Samaselim1@users.noreply.github.com>
2024-05-01 02:47:38 +0300

    Update CardService.java

--- TrelloApp/src/Service/CardService.java
+++ TrelloApp/src/Service/CardService.java
@@ -15,28 +15,47 @@ import javax.persistence.PersistenceContext;
 @Stateless
 public class CardService {
 	
-	@PersistenceContext
+    @PersistenceContext(unitName = "TrelloPU")
     private EntityManager entityManager;
 	
-	 public Card createCard(User user, Lists list, String cardDescription) {
-	        Card card = new Card(cardDescription);
-	        list.addCard(card);
-	        // Implementation
-	        return card;
-	    }
+    public Card createCard(User user, Lists list, String cardDescription) {
+        if (list.getCollaborators().contains(user)) {
+            Card card = new Card(cardDescription);
+            list.addCard(card);
+            entityManager.persist(card);
+            return card;
+        } else {
+            throw new RuntimeException("User does not have permission to create a card in this list.");
+        }
+    }
 
-	    public void moveCard(User user, Card card, List newList) {
-	        // Implementation
-	        // Remove card from current list and add to new list
-	        // Check user role to ensure only appropriate transitions are allowed
-	    }
+    public void moveCard(User user, Card card, Lists newList) {
+        if (card.getList().getCollaborators().contains(user)) {
+            Lists currentList = card.getList();
+            currentList.removeCard(card);
+            newList.addCard(card);
+            card.setList(newList);
+            entityManager.merge(card);
+        } else {
+            throw new RuntimeException("User does not have permission to move the card.");
+        }
+    }
 
-	    public void assignCard(User user, Card card, User assignee) {
-	        // Implementation
-	    }
-
-	    public void addComment(User user, Card card, String comment) {
-	        card.addComment(comment);
-	        // Implementation
-	    }
+    public void assignCard(User user, Card card, User assignee) {
+        if (card.getList().getCollaborators().contains(user)) {
+            card.setOwner(assignee);
+            entityManager.merge(card);
+        } else {
+            throw new RuntimeException("User does not have permission to assign the card.");
+        }
+    }
+    
+    public void addComment(User user, Card card, String comment) {
+        if (card.getList().getCollaborators().contains(user)) {
+            card.addComment(comment);
+            entityManager.merge(card);
+        } else {
+            throw new RuntimeException("User does not have permission to add a comment to the card.");
+        }
+    }
 }
