6090d2b207b479609879811a5eae39f763288513
Samaselim1 <125610375+Samaselim1@users.noreply.github.com>
2024-05-01 02:44:40 +0300

    Update Card.java

--- TrelloApp/src/Model/Card.java
+++ TrelloApp/src/Model/Card.java
@@ -6,7 +6,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Card {
@@ -16,10 +20,26 @@ public class Card {
     private Long id;
 	
     private String description;
+    
+    @OneToMany(mappedBy = "card")
     private List<String> comments;
     
     @ManyToOne
-    private Lists taskList;
+    @JoinColumn(name = "list_id")
+    private Lists list;
+    
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User Owner;
+    
+    @ManyToMany
+    @JoinTable(
+            name = "card_collaborators",
+            joinColumns = @JoinColumn(name = "card_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> collaborators = new ArrayList<>();
+    
     
     public Card(String description) {
         this.description = description;
@@ -32,6 +52,30 @@ public class Card {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public User getOwner() {
+		return Owner;
+	}
+
+	public void setOwner(User owner) {
+		Owner = owner;
+	}
+
+	public List<User> getCollaborators() {
+		return collaborators;
+	}
+
+	public void setCollaborators(List<User> collaborators) {
+		this.collaborators = collaborators;
+	}
+
+	public Lists getList() {
+		return list;
+	}
+
+	public void setList(Lists list) {
+		this.list = list;
 	}
 	
 	public void addComment(String comment) {
