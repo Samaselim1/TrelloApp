769ada2f73bb58dbdce208c327814410a7df9b06
ahmedcanoo <149202243+ahmedcanoo@users.noreply.github.com>
2024-04-30 16:16:14 +0300

    Update Board.java

--- TrelloApp/src/Model/Board.java
+++ TrelloApp/src/Model/Board.java
@@ -7,6 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 @Entity
@@ -16,26 +19,60 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 	
-    private String Boardname;
-    
+    private String boardName;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @ManyToMany(mappedBy = "boards")
+    private List<User> collaborators;
+
+    public Board(String name, User owner) {
+       this. boardName = name;
+        this.owner = owner;
+        this.collaborators = new ArrayList<>();
+    }
     @OneToMany(mappedBy = "board")
-    private List<List> lists;
+    private List<Lists> lists;
 
     public Board(String name) {
-        this.Boardname = name;
+        this.boardName = name;
         this.lists = new ArrayList<>();
     }
     
 	public String getBoardname() {
-		return Boardname;
+		return boardName;
 	}
 
 	public void setBoardname(String boardname) {
-		Boardname = boardname;
+		boardName = boardname;
 	}
 	
-	public void addList(List list) {
+	public void addList(Lists list) {
         lists.add(list);
     }
+	 public User getOwner() {
+	        return owner;
+	    }
+
+	    public void setOwner(User owner) {
+	        this.owner = owner;
+	    }
+	    public List<User> getCollaborators() {
+	        return collaborators;
+	    }
+
+	    public void setCollaborators(List<User> collaborators) {
+	        this.collaborators = collaborators;
+	    }
+	    public void addCollaborator(User user) {
+	        collaborators.add(user);
+	        user.getBoards().add(this);
+	    }
+
+	    public void removeCollaborator(User user) {
+	        collaborators.remove(user);
+	        user.getBoards().remove(this);
+	    }
 
 }
