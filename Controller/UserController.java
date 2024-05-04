e18ac519d718e49947007e46dd981bb7c42c9fe5
Samaselim1 <125610375+Samaselim1@users.noreply.github.com>
2024-05-05 01:59:26 +0300

    Update UserController.java

--- Controller/UserController.java
+++ Controller/UserController.java
@@ -3,6 +3,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -11,14 +12,29 @@ import Service.UserService;
 import Model.User; 
 @Path("/users")
 public class UserController {
-    @POST
+	
+	@Inject
+    private UserService userService;
+	
+	
+	@POST
     @Path("/register")
     @Consumes(MediaType.APPLICATION_JSON)
     public Response registerUser(User user) {
-    	UserService us =new UserService();
-    	us.registerUser(user);
-        return Response.ok().build();
+        userService.registerUser(user);
+        return Response.ok("User registered Successfully").build();
     }
+    
+//    @POST
+//    @Path("/register")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public Response registerUser(User user) {
+//    	UserService us =new UserService();
+//    	us.registerUser(user);
+//        return Response.ok().build();
+//    }
+    
+    
     @POST
     @Path("/login")
     @Consumes(MediaType.APPLICATION_JSON)
