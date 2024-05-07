1ab9a0206d7f2c1a96cd1959ec08abc8eb06ee04
ahmedcanoo <149202243+ahmedcanoo@users.noreply.github.com>
2024-05-08 00:49:08 +0300

    Update BoardService.java

--- Service/BoardService.java
+++ Service/BoardService.java
@@ -31,14 +31,13 @@ public class BoardService {
     public Response createBoard(User user, @PathParam("boardName")String boardName) {
         return boardcontroller.createBoard(user, boardName);
     }
-    @POST
-    @Path("/invite")
+  @POST
+    @Path("/invite/{username}/{boardName}/{username2}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response inviteUser(User user, Board board) {
-    	boardcontroller.inviteUser(user, board);
-        return Response.ok().build();
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response inviteUser(@PathParam("username")String user,@PathParam("boardName")String board, @PathParam("username2")String invitee) {
+    	return boardcontroller.inviteUser(user, board , invitee);
     }
-
     @DELETE
     @Path("/delete")
     @Consumes(MediaType.APPLICATION_JSON)
