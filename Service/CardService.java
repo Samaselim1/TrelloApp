4fd08d0d7d49c7b148abbd4794dfec861ff09a80
DemiNehad <112664092+DemiNehad@users.noreply.github.com>
2024-05-08 18:37:15 +0300

    Update CardService.java

--- Service/CardService.java
+++ Service/CardService.java
@@ -2,54 +2,68 @@ package Service;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import Controller.CardController;
-import Model.Card;
-import Model.Lists;
-import Model.User;
 
 
 @Path("/cards")
 public class CardService {
 
-	    @Inject
+	  @Inject
 	    private CardController cardcontroller;
 
-	@POST
+	  @POST
 	    @Path("/create/{username}/{listName}/{cardDescription}")
 	    @Consumes(MediaType.APPLICATION_JSON)
 	    @Produces(MediaType.APPLICATION_JSON)
 	    public Response createCard(@PathParam("username")String username,@PathParam("listName") String listName, @PathParam("cardDescription")String cardDescription) {
 	        return cardcontroller.createCard(username, listName, cardDescription);
 	      	    }
+	  
 
-	    @PUT
-	    @Path("/move")
-	    @Consumes(MediaType.APPLICATION_JSON)
-	    public Response moveCard(User user, Card card, Lists newList) {
-	    	cardcontroller.moveCard(user, card, newList);
-	        return Response.ok().build();
+	  @POST
+	    @Path("/move/{username}/{cardId}/{targetListName}")
+	    @Produces(MediaType.APPLICATION_JSON)
+	    public Response moveCard(@PathParam("username") String username,
+	                             @PathParam("cardId") Long cardId,
+	                             @PathParam("targetListName") String targetListName) {
+	        return cardcontroller.moveCard(username, cardId, targetListName);
 	    }
 
-	    @PUT
-	    @Path("/assign")
+	  @PUT
+	    @Path("/assign/{username}/{cardId}/{assigneeUsername}")
 	    @Consumes(MediaType.APPLICATION_JSON)
-	    public Response assignCard(User user, Card card, User assignee) {
-	    	cardcontroller.assignCard(user, card, assignee);
-	        return Response.ok().build();
+	    public Response assignCard(@PathParam("username") String username,
+	                               @PathParam("cardId") Long cardId,
+	                               @PathParam("assigneeUsername") String assigneeUsername) {
+	        return cardcontroller.assignCard(username, cardId, assigneeUsername);
+	    }
+	  @PUT
+	    @Path("/add/{cardId}")
+	    @Consumes(MediaType.APPLICATION_JSON)
+	    @Produces(MediaType.APPLICATION_JSON)
+	    public Response addCommentAndDescription(@PathParam("cardId") Long cardId, String requestBody) 
+	    {
+	        return cardcontroller.addCommentAndDescription(cardId, requestBody);
+	    }
+	  
+	  
+	    @GET
+	    @Path("/get/{cardId}")
+	    @Produces(MediaType.APPLICATION_JSON)
+	    @Consumes(MediaType.APPLICATION_JSON)
+
+	    public Response getCard(@PathParam("cardId") Long cardId)
+	    {
+	    	return cardcontroller.getCard(cardId);
 	    }
 
-	    /* public Response addComment(User user, Card card, String comment) {
-	    @PUT
-	    @Path("/comment")
-	    @Consumes(MediaType.APPLICATION_JSON)
-	        cardService.addComment(user, card, comment);
-	        return Response.ok().build();
-	    }*/
 }
