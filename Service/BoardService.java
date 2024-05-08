cdd7150c703c62527202d753a9e88ae796c0e9b1
DemiNehad <112664092+DemiNehad@users.noreply.github.com>
2024-05-08 22:00:54 +0300

    Update BoardService.java

--- Service/BoardService.java
+++ Service/BoardService.java
@@ -8,12 +8,14 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import Controller.BoardController;
 import Model.Board;
+
 import Model.User;
 
 
@@ -24,32 +26,39 @@ public class BoardService {
     @Inject
     private BoardController boardcontroller;
 
-@POST
+    @POST
     @Path("/create/{boardName}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response createBoard(User user, @PathParam("boardName")String boardName) {
         return boardcontroller.createBoard(user, boardName);
     }
-  @POST
+
+    @POST
     @Path("/invite/{username}/{boardName}/{username2}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response inviteUser(@PathParam("username")String user,@PathParam("boardName")String board, @PathParam("username2")String invitee) {
     	return boardcontroller.inviteUser(user, board , invitee);
     }
+
+  
     @DELETE
-    @Path("/delete")
+    @Path("/delete/{boardName}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response deleteBoard(User user, Board board) {
+    public Response deleteBoard(User user, @PathParam("boardName")String board) {
         return boardcontroller.deleteBoard(user, board);
     }
 
     @GET
-    @Path("/all")
+    @Path("/all/{username}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getAllBoards(User user) {
-        List<Board> boards = boardcontroller.getAllBoards(user);
+    public Response getAllBoards(@PathParam("username") String username) {
+        List<Board> boards = boardcontroller.getAllBoards(username);
+        if (boards.isEmpty()) {
+            return Response.status(Response.Status.NOT_FOUND).entity("No boards found or user not found").build();
+        }
         return Response.ok().entity(boards).build();
     }
+
 }
