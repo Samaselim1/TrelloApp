f44a2d1ff8d28331057b28cb5e842c9f8195d9b9
DemiNehad <112664092+DemiNehad@users.noreply.github.com>
2024-05-08 18:38:33 +0300

    Update Card.java

--- Model/Card.java
+++ Model/Card.java
@@ -2,7 +2,10 @@ package Model;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -10,7 +13,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-//import javax.persistence.OneToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 
 @Entity
 public class Card {
@@ -20,10 +24,15 @@ public class Card {
     private Long id;
 	
     private String description;
+  private String comment;
     
-   // @OneToMany(mappedBy = "card")
-    //private List<String> comments;
+
     
+    
+  @OneToMany(mappedBy = "card", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+  private List<Comment> comments = new ArrayList<>();
+  
+  
     @ManyToOne
     @JoinColumn(name = "list_id")
     private Lists list;
@@ -38,12 +47,13 @@ public class Card {
             joinColumns = @JoinColumn(name = "card_id"),
             inverseJoinColumns = @JoinColumn(name = "user_id")
     )
+    @Transient
     private List<User> collaborators = new ArrayList<>();
     
+    public Card() {}
     
     public Card(String description) {
         this.description = description;
-       // this.comments = new ArrayList<>();
     }
     
 	public String getDescription() {
@@ -61,7 +71,11 @@ public class Card {
 	public void setOwner(User owner) {
 		Owner = owner;
 	}
+	public List<Comment> getComments() {
+		return comments;
+	}
 
+	
 	public List<User> getCollaborators() {
 		return collaborators;
 	}
@@ -74,12 +88,34 @@ public class Card {
 		return list;
 	}
 
-	public void setList(Lists list) {
-		this.list = list;
-	}
+	public String getComment() {
+        return comment;
+    }
 	
-	/*public void addComment(String comment) {
-        comments.add(comment);
-    }*/
+	public void setComments(List<Comment> comments)
+	{
+		this.comments =comments;
+	}
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+	public void setList(Lists newList) {
+	    if (this.list != null) {
+	        this.list.getCards().remove(this);  
+	    }
+	    this.list = newList;
+	    if (newList != null && !newList.getCards().contains(this)) {
+	        newList.getCards().add(this);  
+	    }
+	}
+	 
+	
+	public void addComment(String commentText) {
+		Comment comment = new Comment();
+		comment.setText(commentText);
+		comment.setCard(this);
+		this.comments.add(comment);
+	}
 
 }
