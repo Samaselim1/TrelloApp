eadf0242fea4c56ea2281d1b89b30ed10494d309
Samaselim1 <125610375+Samaselim1@users.noreply.github.com>
2024-07-08 17:38:15 +0300

    Update User.java

--- TrelloApp/src/Model/User.java
+++ TrelloApp/src/Model/User.java
@@ -4,53 +4,82 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 
 @Entity
 public class User {
 
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long userid;
 	
 	 private String username;
 	 private String password;
 	 private String email;
 	 private String role;
 	 
+	 @Transient
 	 @ManyToMany(mappedBy = "collaborators")
-	    private List<Board> boards;
+	    private List<Board> boards = new ArrayList<>();
 
+	 @Transient
+	 @OneToMany(mappedBy = "owner", fetch = FetchType.EAGER)
+	    private List<Board> ownedBoards= new ArrayList<>();
 	 
-	 public User(String email, String password, String name) {
-	        this.email = email;
+	 public User( ) {};
+	 public User(String email, String password, String name , String role) {
+		 	this.email = email;
 	        this.password = password;
 	        this.username = name;
-	        this.boards = new ArrayList<>();
-
+	        this.role = role;
 	    }
+	
+	 public void setId(Long id)
+	 {
+		 this.userid = id;
+	 }
+	 
+	 public Long getId()
+	 {
+		 return userid;
+	 }
+	 
+	 public String getUsername() {
+		 return username;
+	 }
+	 public void setUsername(String name) {
+		 this.username = name;
+	 }
+	 
+	 public String getPassword() {
+		 return password;
+	 }
+	 public void setPassword(String password) {
+		 this.password = password;
+	 }
+	 
+	 public void setEmail(String email) {
+		 this.email = email;
+	 }
 	 
 	public String getEmail() {
 		return email;
 	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getName() {
-		return username;
-	}
-	public void setName(String name) {
-		this.username = name;
-	}
+	
+	public List<Board> getOwnedBoards() {
+        return ownedBoards;
+    }
+
+    public void setOwnedBoards(List<Board> ownedBoards) {
+        this.ownedBoards = ownedBoards;
+    }
+	
 	public String getRole() {
 		return role;
 	}
