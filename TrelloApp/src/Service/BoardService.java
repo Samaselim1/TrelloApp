f449f8c428a7c0b0bccd2251162bba14b3a92319
Samaselim1 <125610375+Samaselim1@users.noreply.github.com>
2024-07-08 17:45:18 +0300

    Update BoardService.java

--- TrelloApp/src/Service/BoardService.java
+++ TrelloApp/src/Service/BoardService.java
@@ -1,43 +1,64 @@
 package Service;
 
-import Model.User;
-import Model.Board;
-
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import java.util.List;
 
-@Stateless
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import Controller.BoardController;
+import Model.Board;
+
+import Model.User;
+
+
+@Path("/boards")
 public class BoardService {
-	
-	@PersistenceContext(unitName="TrelloPU")
-    private EntityManager entityManager;
-	
-	public Board createBoard(User user, String boardName) {
-        Board board = new Board(boardName);
-        board.setOwner(user); 
-        entityManager.persist(board); 
-        return board;
+
+
+    @Inject
+    private BoardController boardcontroller;
+
+    @POST
+    @Path("/create/{boardName}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createBoard(User user, @PathParam("boardName")String boardName) {
+        return boardcontroller.createBoard(user, boardName);
     }
 
-    public void inviteUser(User user, Board board) {
-        board.getCollaborators().add(user); 
-        entityManager.merge(board); 
+    @POST
+    @Path("/invite/{username}/{boardName}/{username2}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response inviteUser(@PathParam("username")String user,@PathParam("boardName")String board, @PathParam("username2")String invitee) {
+    	return boardcontroller.inviteUser(user, board , invitee);
     }
 
-    public void deleteBoard(User user, Board board) {
-        if (user.equals(board.getOwner())) {
-            entityManager.remove(board); 
-        } else {
-            throw new RuntimeException("User does not have permission to delete the board");
+  
+    @DELETE
+    @Path("/delete/{boardName}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response deleteBoard(User user, @PathParam("boardName")String board) {
+        return boardcontroller.deleteBoard(user, board);
+    }
+
+    @GET
+    @Path("/all/{username}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllBoards(@PathParam("username") String username) {
+        List<Board> boards = boardcontroller.getAllBoards(username);
+        if (boards.isEmpty()) {
+            return Response.status(Response.Status.NOT_FOUND).entity("No boards found or user not found").build();
         }
+        return Response.ok().entity(boards).build();
     }
 
-    public List<Board> getAllBoards(User user) {
-        Query query = entityManager.createQuery("SELECT b FROM Board b WHERE b.owner = :user OR :user MEMBER OF b.collaborators");
-        query.setParameter("user", user);
-        return query.getResultList();
-    }
 }
