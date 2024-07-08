0df5dd37efad06b9877dcc7f695ad463dfe60a16
Samaselim1 <125610375+Samaselim1@users.noreply.github.com>
2024-07-08 17:46:02 +0300

    Update ListService.java

--- TrelloApp/src/Service/ListService.java
+++ TrelloApp/src/Service/ListService.java
@@ -1,40 +1,55 @@
 package Service;
 
-import Model.User;
-import Model.Board;
-import Model.Lists;
 
-import java.util.List;
-
-import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 
-@Stateless
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import Controller.ListController;
+
+
+
+@Path("/lists")
 public class ListService {
-	
+
+    @Inject
+    private ListController listcontroller;
+
     @PersistenceContext(unitName = "TrelloPU")
     private EntityManager entityManager;
-	
-	 public Lists createList(User user, Board board, String listName) {
-		 Lists list = new Lists(listName, board, user);
-		 list.setOwner(user);
-		 entityManager.persist(list);
-	        return list;
-	 }
+    
+    @POST
+    @Path("/create/{listName}/{boardName}/{user}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+	 public Response createList(@PathParam("listName")String listName, @PathParam("boardName") String boardName,@PathParam("user") String user) {
+        try {
+            return listcontroller.createList(listName, boardName, user);
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Failed to create list").build();
+        }
+    }
 
-	 public void deleteList(User user, Lists list) {
-	        if (user.equals(list.getOwner())) {
-	            entityManager.remove(entityManager.merge(list));
-	        } else {
-	            throw new RuntimeException("Only TeamLeader can delete lists.");
-	        }
-	    }
-	    
-	    public List<Lists> getAllLists(Board board) {
-	        Query query = entityManager.createQuery("SELECT l FROM Lists l WHERE l.board = :board");
-	        query.setParameter("board", board);
-	        return query.getResultList();
-	    }
+   
+    @DELETE
+    @Path("/delete/{listName}/{user}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteList(@PathParam("listName") String Listname, @PathParam("user") String user) {
+        try {
+            return listcontroller.deleteList(Listname, user);
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Failed to delete list: " + e.getMessage()).build();
+        }
+    }
+
+ 
+
 }
