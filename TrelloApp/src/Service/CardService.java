854642aeeea55f0e8317b1abf4769f7e8725bd97
Samaselim1 <125610375+Samaselim1@users.noreply.github.com>
2024-07-08 17:45:43 +0300

    Update CardService.java

--- TrelloApp/src/Service/CardService.java
+++ TrelloApp/src/Service/CardService.java
@@ -1,61 +1,67 @@
 package Service;
 
-import Model.User;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-import java.util.List;
+import Controller.CardController;
 
-import Model.Board;
-import Model.Card;
-import Model.Lists;
 
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-@Stateless
+@Path("/cards")
 public class CardService {
-	
-    @PersistenceContext(unitName = "TrelloPU")
-    private EntityManager entityManager;
-	
-    public Card createCard(User user, Lists list, String cardDescription) {
-        if (list.getCollaborators().contains(user)) {
-            Card card = new Card(cardDescription);
-            list.addCard(card);
-            entityManager.persist(card);
-            return card;
-        } else {
-            throw new RuntimeException("User does not have permission to create a card in this list.");
-        }
-    }
 
-    public void moveCard(User user, Card card, Lists newList) {
-        if (card.getList().getCollaborators().contains(user)) {
-            Lists currentList = card.getList();
-            currentList.removeCard(card);
-            newList.addCard(card);
-            card.setList(newList);
-            entityManager.merge(card);
-        } else {
-            throw new RuntimeException("User does not have permission to move the card.");
-        }
-    }
+	  @Inject
+	    private CardController cardcontroller;
 
-    public void assignCard(User user, Card card, User assignee) {
-        if (card.getList().getCollaborators().contains(user)) {
-            card.setOwner(assignee);
-            entityManager.merge(card);
-        } else {
-            throw new RuntimeException("User does not have permission to assign the card.");
-        }
-    }
-    
-    public void addComment(User user, Card card, String comment) {
-        if (card.getList().getCollaborators().contains(user)) {
-            card.addComment(comment);
-            entityManager.merge(card);
-        } else {
-            throw new RuntimeException("User does not have permission to add a comment to the card.");
-        }
-    }
+	  @POST
+	    @Path("/create/{username}/{listName}/{cardDescription}")
+	    @Consumes(MediaType.APPLICATION_JSON)
+	    @Produces(MediaType.APPLICATION_JSON)
+	    public Response createCard(@PathParam("username")String username,@PathParam("listName") String listName, @PathParam("cardDescription")String cardDescription) {
+	        return cardcontroller.createCard(username, listName, cardDescription);
+	      	    }
+	  
+
+	  @POST
+	    @Path("/move/{username}/{cardId}/{targetListName}")
+	    @Produces(MediaType.APPLICATION_JSON)
+	    public Response moveCard(@PathParam("username") String username,
+	                             @PathParam("cardId") Long cardId,
+	                             @PathParam("targetListName") String targetListName) {
+	        return cardcontroller.moveCard(username, cardId, targetListName);
+	    }
+
+	  @PUT
+	    @Path("/assign/{username}/{cardId}/{assigneeUsername}")
+	    @Consumes(MediaType.APPLICATION_JSON)
+	    public Response assignCard(@PathParam("username") String username,
+	                               @PathParam("cardId") Long cardId,
+	                               @PathParam("assigneeUsername") String assigneeUsername) {
+	        return cardcontroller.assignCard(username, cardId, assigneeUsername);
+	    }
+	  @PUT
+	    @Path("/add/{cardId}")
+	    @Consumes(MediaType.APPLICATION_JSON)
+	    @Produces(MediaType.APPLICATION_JSON)
+	    public Response addCommentAndDescription(@PathParam("cardId") Long cardId, String requestBody) 
+	    {
+	        return cardcontroller.addCommentAndDescription(cardId, requestBody);
+	    }
+	  
+	
+	  
+	    @PUT
+	    @Path("/update/{cardId}")
+	    @Consumes(MediaType.APPLICATION_JSON)
+	    @Produces(MediaType.APPLICATION_JSON)
+	    public Response updateCard(@PathParam("cardId") Long cardId, String requestBody)
+	                               {
+	        return cardcontroller.updateCard( cardId, requestBody);
+	    }
 }
